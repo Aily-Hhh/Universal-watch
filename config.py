d8b69a814f43a2297fab3fba0cf5b5fbce6f73f9
Aily-Hhh <aily.hhh13@gmail.com>
2022-03-27 04:14:21 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+
+DIR = Path(__file__).resolve().parent
+SETTINGS_FILE_NAME = str(DIR / 'settings.ini')
+DIR_ICONS = DIR / 'icons'
---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPalette, QColor, QFont
+from PyQt5.QtWidgets import QApplication
+from UserInt.mainwindow import MainWindow
+
+app = QApplication(sys.argv)
+
+app.setFont(QFont("Century Gothic", 10))
+
+mw = MainWindow()
+palette = QPalette()
+palette.setColor(QPalette.Window, QColor(152, 251, 152))
+mw.setPalette(palette)
+mw.show()
+
+sys.exit(app.exec_())
